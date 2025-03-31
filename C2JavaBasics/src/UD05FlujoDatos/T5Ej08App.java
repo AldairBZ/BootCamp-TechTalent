de45ebd93fcd878952095a02d5e63a3e75b46392
Scheneider Aldair <aldairbz1719@gmail.com>
2025-03-31 11:33:56 +0200

    TareasUD05
    12 tareas se hicieron

---
+++ C2JavaBasics/src/UD05FlujoDatos/T5Ej08App.java
@@ -0,0 +1,17 @@
+package UD05FlujoDatos;
+
+
+import java.util.Iterator;
+
+@SuppressWarnings("unused")
+public class T5Ej08App {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
