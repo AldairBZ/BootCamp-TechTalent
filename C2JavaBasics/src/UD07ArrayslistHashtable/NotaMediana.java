3e851b013a4da0315a970abdf2100aa476656c48
AldairBZ <aldairbz1719@gmail.com>
2025-04-23 12:11:07 +0200

    ss

--- C2JavaBasics/src/UD07ArrayslistHashtable/NotaMediana.java
+++ C2JavaBasics/src/UD07ArrayslistHashtable/NotaMediana.java
@@ -8,8 +8,8 @@ public class NotaMediana {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		Map<String, ArrayList<Double>> notesAlumnes = demanarNotes(scanner); 
-		Map<String, Double> mitjanesAlumnes = calcularMitjanes(notesAlumnes); 
+		Map<String, ArrayList<Double>> notasAlumnos = demanarNotes(scanner); 
+		Map<String, Double> mitjanesAlumnes = calcularMitjanes(notasAlumnos); 
 		mostrarMitjanes(mitjanesAlumnes);
 
 		scanner.close();
@@ -19,20 +19,20 @@ public class NotaMediana {
 	public static Map<String, ArrayList<Double>> demanarNotes(Scanner scanner) {
 		Map<String, ArrayList<Double>> dades = new HashMap<>();
 
-		System.out.print("Quants alumnes vols introduir? ");
+		System.out.print("cuantos alumnos vas a introducir? ");
 		int numAlumnes = scanner.nextInt();
 		scanner.nextLine(); // Neteja buffer
 
 		for (int i = 0; i < numAlumnes; i++) {
-			System.out.print("Nom de l'alumne " + (i + 1) + ": ");
+			System.out.print("nombre del alumno " + (i + 1) + ": ");
 			String nom = scanner.nextLine();
 
 			ArrayList<Double> notes = new ArrayList<>();
-			System.out.print("Quantes notes té " + nom + "? ");
+			System.out.print("cuantas notas tiene " + nom + "? ");
 			int numNotes = scanner.nextInt();
 
 			for (int j = 0; j < numNotes; j++) {
-				System.out.print("Introdueix la nota " + (j + 1) + ": ");
+				System.out.print("introducir la nota " + (j + 1) + ": ");
 				double nota = scanner.nextDouble();
 				notes.add(nota);
 			}
@@ -67,7 +67,7 @@ public class NotaMediana {
 
 	// Mètode per mostrar les mitjanes
 	public static void mostrarMitjanes(Map<String, Double> mitjanes) {
-		System.out.println("\n--- Nota mitjana dels alumnes ---");
+		System.out.println("\n--- Nota mediana de alumno ---");
 		for (Map.Entry<String, Double> entrada : mitjanes.entrySet()) {
 			System.out.printf("%s 🡆 %.2f%n", entrada.getKey(), entrada.getValue());
 		}
