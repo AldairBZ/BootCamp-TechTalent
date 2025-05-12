f374baf916d18de354545920275c45f2a348e837
Scheneider Aldair <aldairbz1719@gmail.com>
2025-05-12 12:31:37 +0200

    ss

---
+++ C2JavaBasics/src/UD09Herencias/Entregables/Entregable.java
@@ -0,0 +1,12 @@
+package UD09Herencias.Entregables;
+
+public class Entregable {
+	void entregar();
+
+	void devolver();
+
+	boolean isEntregad0();
+
+	int compareTo(objeto a); // compara duracion (horas o temporadas)
+
+}
