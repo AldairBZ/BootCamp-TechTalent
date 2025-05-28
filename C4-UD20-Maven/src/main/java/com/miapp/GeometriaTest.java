c82e8d92e100e0b3ff88b2bad66126d74af8789f
Scheneider Aldair <aldairbz1719@gmail.com>
2025-05-28 13:11:41 +0200

    new

--- C4-UD20-Maven/src/main/java/com/miapp/GeometriaTest.java
+++ C4-UD20-Maven/src/main/java/com/miapp/GeometriaTest.java
@@ -1,7 +1,6 @@
 package com.miapp;
-
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GeometriaTest {
 
